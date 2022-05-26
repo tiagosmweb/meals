96887ca8e95bbd0418fd4edf37ddbc2ac985be64
Tiago Web <tiagoweb@hotmail.com>
2022-05-26 19:17:43 -0400

    add tratamento string data

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -40,4 +40,30 @@ class Meal {
     required this.complexity,
     required this.cost,
   });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Difficult:
+        return 'Difícil';
+      case Complexity.Medium:
+        return 'Normal';
+      case Complexity.Simple:
+        return 'Simples';
+      default:
+        return 'Desconhecida';
+    }
+  }
+
+  String get costText {
+    switch (cost) {
+      case Cost.Cheap:
+        return 'Barato';
+      case Cost.Fair:
+        return 'Justo';
+      case Cost.Expensive:
+        return 'Caro';
+      default:
+        return 'Desconhecido';
+    }
+  }
 }
