04771a9135bb4135240afc0d6bc867f45e6f12b8
Tiago Web <tiagoweb@hotmail.com>
2022-05-26 16:42:13 -0400

    create screen simples categorie meal e navigation

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../screens/categories_meals.dart';
 
 class CategoryItem extends StatelessWidget {
   // const MyWidget({Key? key}) : super(key: key);
@@ -8,24 +9,36 @@ class CategoryItem extends StatelessWidget {
   final Category category;
   const CategoryItem(this.category);
 
+  void _selectedCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoriesMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectedCategory(context),
+      splashColor: Theme.of(context).colorScheme.primary,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
     );
   }
---
+++ lib/screens/categories_meals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Receitas'),
+      ),
+      body: const Center(
+        child: Text('Receita por categoria'),
+      ),
+    );
+  }
+}
