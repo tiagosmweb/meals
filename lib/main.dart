31a964aed99f5d3b858ea1a075f829ca6ca95e2c
Tiago Web <tiagoweb@hotmail.com>
2022-05-27 17:45:29 -0400

    add filter

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,38 @@ import 'screens/categories.dart';
 import 'screens/categories_meals.dart';
 import 'screens/filters.dart';
 import 'utils/app_routes.dart';
+import 'models/meal.dart';
+import 'models/filters.dart';
+import 'data/dummy_data.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Filters filters = Filters();
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _filterMeals(Filters filters) {
+    setState(() {
+      this.filters = filters;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        final filterGluten = filters.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = filters.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = filters.isVegan && !meal.isVegan;
+        final filterVegetarian = filters.isVegetarian && !meal.isVegetarian;
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,9 +56,10 @@ class MyApp extends StatelessWidget {
       // home: CategorieScreen(),
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAILS: (ctx) => MealDetailsScreen(),
-        AppRoutes.FILTERS: (ctx) => FiltersScreen(),
+        AppRoutes.FILTERS: (ctx) => FiltersScreen(filters, _filterMeals),
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 import '../components/main_drawer.dart';
+import '../models/filters.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
+  final Filters filters;
+  final Function(Filters) onFiltersChanged;
+
+  const FiltersScreen(this.filters, this.onFiltersChanged);
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  late Filters filters;
+
+  @override
+  void initState() {
+    super.initState();
+    filters = widget.filters;
+  }
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function(bool) onChanged,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: (value) {
+        onChanged(value);
+        widget.onFiltersChanged(filters);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,8 +45,46 @@ class FiltersScreen extends StatelessWidget {
         title: const Text('Filtros'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: const Text('Filtros!'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _createSwitch(
+                  'Sem Glutén',
+                  'Lista refeições sem glutén',
+                  filters.isGlutenFree,
+                  (value) => setState(() => filters.isGlutenFree = value),
+                ),
+                _createSwitch(
+                  'Sem Lactose',
+                  'Lista refeições sem Lactose',
+                  filters.isLactoseFree,
+                  (value) => setState(() => filters.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegana',
+                  'Lista  refeições Vegana',
+                  filters.isVegan,
+                  (value) => setState(() => filters.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetariana',
+                  'Lista refeições Vegetarianas',
+                  filters.isVegetarian,
+                  (value) => setState(() => filters.isVegetarian = value),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
